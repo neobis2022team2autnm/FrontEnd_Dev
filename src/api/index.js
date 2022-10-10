58b54f125131dc0388da6e1ace2867c16ce76216
neobis2022team2autnm <project.autumn.2022.team2@gmail.com>
2022-10-10 18:04:34 +0600

    refactor: add folder for work with api and create mock data for work

---
+++ src/api/index.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { doctorsList } from "./mock/doctorsMock";
+
+const instance = axios.create({
+  baseURL: "https://testrepobackend.herokuapp.com/",
+  headers: {
+    ContentType: "application/json",
+  },
+});
+
+export const authApi = {
+  login: (data) => {
+    return instance.post("authenticate/login/", data);
+  },
+};
+
+// Пациенты
+// CRUD - CREATE READ UPDATE DELETE
+
+// Врачи
+export const doctorsApi = {
+  getDoctorsList: () => {
+    try {
+      return instance.get("doctors").catch((e) => {
+        return Promise.resolve(doctorsList);
+      });
+    } catch (e) {
+      return Promise.resolve(doctorsList);
+    }
+  },
+  createDoctor: () => {
+    return instance.post("doctors");
+  },
+  updateDoctors: () => {
+    return instance.put("doctors");
+  },
+  deleteDoctor: () => {
+    return instance.delete("doctors");
+  },
+};
+
+// Документы
+
+// Чеклисты
