5346db9cef2a2e4f6f2227a16762ebb0de36d912
neobis2022team2autnm <project.autumn.2022.team2@gmail.com>
2022-10-14 21:21:08 +0600

    refactor:change title in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between bg-gray-200 w-full p-4">
       <h1 className="text-center text-2xl font-bold">
-        Firebase Google Auth & Context
+   SilkRoad Trip
       </h1>
       {user?.displayName ? (
         <button onClick={handleSignOut}>Logout</button>
