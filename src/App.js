9143a90aea8f4ac1534e5c217aa63e273a24389e
neobis2022team2autnm <project.autumn.2022.team2@gmail.com>
2022-10-18 13:55:38 +0600

    feat: implemented google login functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
 
 function App() {
-  return <div>Hello</div>;
+  const [user, setUser] = useState({});
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token" + response.credential);
+    let userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSingOut(e) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  useEffect(() => {
+    /*global google */
+    google.accounts.id.initialize({
+      client_id:
+        "389304183889-m72ksip97kttaov9jkd86anlkkgiphgh.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+
+    google.accounts.id.prompt();
+  }, []);
+
+  return (
+    <div>
+      <div id="signInDiv"></div>
+      {Object.keys(user).length != 0 && (
+        <button onClick={(e) => handleSingOut(e)}>Выход</button>
+      )}
+      {user && (
+        <div>
+          <img src={user.picture}></img>
+          <h3>{user.name}</h3>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
