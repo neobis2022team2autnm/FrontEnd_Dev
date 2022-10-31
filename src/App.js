9f70a9485fd043d0905fd9231088737ada8960b7
codeSlayer7 <bahazve01@gmail.com>
2022-10-31 20:45:36 +0600

    feat: add navigation buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Navigation from './components/Navigation';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Navigation/>
     </div>
   );
 };
