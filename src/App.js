467521e2c2b17e683af3248afbc51d00137d50b0
codeSlayer7 <bahazve01@gmail.com>
2022-11-08 18:27:10 +0600

    feat: create search-bar

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Home from './pages/Home';
 import Navigation from "./components/Navigation";
 import Slider from "./components/Slider";
 import Footer from "./components/Footer";
+import SearchBar from "./components/Search-bar";
 
 const App = () => {
   return (
-    <div className="w-screen px=[1%] md:px-[5%] mx-auto">
+    <div className="w-screen px=[1%] md:px-[5%] lg:px-[15%] mx-auto">
       <Navbar />
       <Navigation />
+      <SearchBar/>
       <Slider  />
       <Routes>
         {/* <Route path="/*" element={<Home />} /> */}
