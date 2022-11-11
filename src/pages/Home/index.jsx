e207677cfeaedeb2fa45e9af32aca10f143e2641
neobis2022team2autnm <project.autumn.2022.team2@gmail.com>
2022-11-11 18:30:40 +0600

    fix: fixing photo and routes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -45,13 +45,13 @@ const Home = () => {
 
   return (
     <div>
-      <Slider  title={t('want_to_know_more')} id="slider-1" />
-      <Slider  title={t('popular_articles')} id="slider-2" />
-      <Slider  title={t('places_where_i_can_eat_good_food')}  id="slider-3" />
-      <Slider  title={t('you_can_have_a_good_time_here')} id="slider-4" />
       <div className="board">
         {isLogged ? <Board user={user} logout={handleLogout} /> : <Unlogged />}
       </div>
+      <Slider title={t("want_to_know_more")} id="slider-1" />
+      <Slider title={t("popular_articles")} id="slider-2" />
+      <Slider title={t("places_where_i_can_eat_good_food")} id="slider-3" />
+      <Slider title={t("you_can_have_a_good_time_here")} id="slider-4" />
     </div>
   );
 };
